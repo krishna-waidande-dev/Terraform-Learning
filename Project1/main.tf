483b4b95562a2c9534480fb0c181efb48499b6ba
krishna-waidande-dev <krishnawaidande.dev@gmail.com>
2021-02-01 18:57:01 +0530

    Adding code to create ec2 free tier instance with AWS provider

---
+++ Project1/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "********"
+  secret_key = "********"
+}
+
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-00ddb0e5626798373"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "tf-first-instance"
+  }
+}
