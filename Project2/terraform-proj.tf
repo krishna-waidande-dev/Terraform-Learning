a6a3214c841690a220ad674aa64cc726d3706cec
krishna-waidande-dev <67575498+krishna-waidande-dev@users.noreply.github.com>
2021-02-02 13:32:33 +0530

    Formatted the script

--- Project2/terraform-proj.tf
+++ Project2/terraform-proj.tf
@@ -1,10 +1,11 @@
+# Provider information
 provider "aws" {
   region = "us-east-1"
   access_key = "***********"
   secret_key = "***********"
 }
 
-#1 Create vpc
+#1 Create VPC
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -35,8 +36,8 @@ resource "aws_route_table" "prod-route-table" {
     Name = "prod"
   }
 }
-#4 Create subnet
 
+#4 Create a Subnet
 resource "aws_subnet" "subnet-1" {
   vpc_id = aws_vpc.prod-vpc.id
   cidr_block = "10.0.1.0/24"
@@ -46,15 +47,13 @@ resource "aws_subnet" "subnet-1" {
   }
 }
 
-#5 Associate subnet with Route table
-
+#5 Associate Subnet with Route table
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet-1.id
   route_table_id = aws_route_table.prod-route-table.id
 }
 
 #6 Create Security Group to allow port 22,80,443
-
 resource "aws_security_group" "allow_web" {
   name        = "allow_web_traffic"
   description = "Allow web traffic"
@@ -95,16 +94,15 @@ resource "aws_security_group" "allow_web" {
     Name = "allow_web"
   }
 }
-#7 Create a network interface with an ip in the subnet that was created in step4
 
+#7 Create a network interface with an ip in the subnet that was created in step#4.
 resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.subnet-1.id
   private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_web.id]
 }
 
-#8 Assign an elastic IP to the network interface created in step 7
-
+#8 Assign an elastic IP to the network interface created in step #7
 resource "aws_eip" "one" {
   vpc                       = true
   network_interface         = aws_network_interface.web-server-nic.id
@@ -112,7 +110,7 @@ resource "aws_eip" "one" {
   depends_on = [aws_internet_gateway.gw]
 }
 
-#9 Create Ubuntu server and install/enable apache2
+#9 Create Ubuntu server and install/enable Apache2.
 resource "aws_instance" "web-server-instance" {
     ami = "ami-00ddb0e5626798373"
     instance_type = "t2.micro"
